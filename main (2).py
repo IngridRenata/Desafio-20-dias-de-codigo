23910a1fec0ffe971bf59e458b8cf5b8d8544706
Ingrid Renata <ingridrenataestudos08@gmail.com>
2026-03-06 14:49:02 -0300

    Dia 3
    exercicio 1002 e 1003

---
+++ main (2).py
@@ -0,0 +1,13 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+n = 3.14159
+raio = float(input())
+
+area = n * (raio**2)
+print(f"A={area:.4f}")
